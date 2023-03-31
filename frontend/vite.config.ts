459564a476bcc0f60b9e5a9152baccdff495e6f7
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-03-31 13:21:07 +1000

    update deps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { warmup } from "vite-plugin-warmup";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  publicDir: "public"
-})
+  publicDir: "public",
+  plugins: [
+    svelte(),
+    warmup({
+      // warm up the files and its imported JS modules recursively
+      clientFiles: ["./src/**/*.cy.ts", "./cypress/support/component.ts"],
+    }),
+  ],
+});
